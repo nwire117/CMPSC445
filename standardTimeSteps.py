15cc238330370763449e61d9b85436ae823f21ae
Jason Cross <49740641+jasonmcross@users.noreply.github.com>
2022-04-14 14:47:28 -0400

    Add files via upload

--- standardTimeSteps.py
+++ standardTimeSteps.py
@@ -36,11 +36,12 @@ lookBack = 5                # 1-100 percent of training data
 lstmNeurons = 4             # any number, but more is not always better
 denseNeurons = 2            # any number, does not need to be used, higher is not always better
 numEpochs = 100             # any number, higher is better, but also longer
+titleOfTest = "Title"       # Unique title of final graph for reference or comparison
 
 '''
 This is only used for testing, can be removed for final submission
 '''
-# fix random seed for reproducibility
+# Fix random seed for reproducibility
 np.random.seed(7)
 
 #ticker = input("Enter ticker: ")
@@ -49,26 +50,35 @@ np.random.seed(7)
 #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker +'&outputsize=full&apikey=RCURB8MB4MUMOVQT&datatype=csv'
 #names = ['date', 'open', 'high', 'low', 'close', 'volume']
 
+# Read in the csv
 df = pd.read_csv('daily_aapl.csv')
 
+# Parsing out the good stuff
 df = df.loc[::-1].reset_index(drop=True)
 dfParsed = df[['timestamp', 'open', 'close']]
 
+# Converting date from string to datetime format
 dfParsed['timestamp'] = pd.to_datetime(df['timestamp'])
+
+# Removing timestamp column and making it the index
 extracted = dfParsed['timestamp']
 dfParsed.set_index('timestamp', drop = True, inplace = True)
 
+# Creating a new dataframe with the intra day change in price
 openPrice = dfParsed['open'].values
 closePrice = dfParsed['close'].values
 intradayChange = openPrice - closePrice
 dfIntradayChange=pd.DataFrame(intradayChange, columns=['Change'])
 
+# Creating a Date column and setting as the index
 dfIntradayChange.insert(0, 'Date', extracted)
 dfIntradayChange.set_index('Date', drop = True, inplace = True)
-upDown = []
 
+# Shrinking the dataset to work with it quicker
 dfIntradayChangeAbridged = dfIntradayChange[int(len(dfIntradayChange) * ((100-sizeOfDataset)/100)):]
 
+# Creating a new array for simpler price movement categorization (not used at the moment)
+upDown = []
 for i in range(len(intradayChange)):
     if intradayChange[i] > 0:
         upDown.append(1)
@@ -77,22 +87,24 @@ for i in range(len(intradayChange)):
     else:
         upDown.append(0)
 
-
+# Turning the array into a dataframe, creating a Date column, and setting it to index
 dfUpDown = pd.DataFrame(upDown, columns = ['Up/Down'])
 dfUpDown.insert(0, 'Date', extracted)
 dfUpDown.set_index('Date', drop = True, inplace = True)
 
+# Plotting for some visual reference while testing
 '''
 plt.plot(intradayChange)
 plt.xlabel("intradayChange")
 plt.show()
 '''
 
-# normalize the dataset
+# Normalizing the dataset 2 different ways to test against each other
 scaler = MinMaxScaler(feature_range=(0, 1))
 dfMMS = scaler.fit_transform(dfIntradayChangeAbridged)
 dfStandard = StandardScaler().fit_transform(dfIntradayChangeAbridged)
 
+# Plotting the different standardizations for visual reference
 '''
 plt.plot(dfMMS)
 plt.xlabel("dfMMS")
@@ -103,13 +115,13 @@ plt.xlabel("dfStandard")
 plt.show()
 '''
 
-# split into train and test sets
+# Split into train and test sets
 train_size = int(len(dfStandard) * (trainingSize/100))
 test_size = len(dfStandard) - train_size
 train, test = dfStandard[0:train_size,:], dfStandard[train_size:len(dfStandard),:]
 print(len(train), len(test))
 
-# convert an array of values into a dataset matrix
+# Convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
@@ -118,16 +130,16 @@ def create_dataset(dataset, look_back):
 		dataY.append(dataset[i + look_back, 0])
 	return np.array(dataX), np.array(dataY)
 
-# reshape into X=t and Y=t+1
+# Reshape into X=t and Y=t+1
 look_back = lookBack
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
-# reshape input to be [samples, time steps, features]
+# Reshape input to be [samples, time steps, features]
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
 testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 
-# create and fit the LSTM network
+# Create and fit the LSTM network
 batch_size = 1
 model = tf.keras.Sequential()
 #model.add(LSTM(16, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
@@ -139,38 +151,38 @@ for i in range(int(numEpochs)):
 	model.fit(trainX, trainY, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
 	model.reset_states()
 
-# make predictions
+# Make predictions
 trainPredict = model.predict(trainX, batch_size=batch_size)
 model.reset_states()
 testPredict = model.predict(testX, batch_size=batch_size)
 
-# invert predictions
+# Invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
-# calculate root mean squared error
+# Calculate root mean squared error
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
-# shift train predictions for plotting
+# Shift train predictions for plotting
 trainPredictPlot = np.empty_like(dfStandard)
 trainPredictPlot[:, :] = np.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
 
-# shift test predictions for plotting
+# Shift test predictions for plotting
 testPredictPlot = np.empty_like(dfStandard)
 testPredictPlot[:, :] = np.nan
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dfStandard)-1, :] = testPredict
 
-# plot baseline and predictions
+# Plot baseline and predictions
 plt.plot(scaler.inverse_transform(dfStandard))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
-plt.xlabel("dfStandard lookback5")
+plt.xlabel(titleOfTest)
 plt.show()
 
 

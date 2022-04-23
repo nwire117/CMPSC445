0bed2a1709fedb705473c09474383c4a80448821
Anthony Pachco <ajp6548@psu.edu>
2022-04-23 13:07:50 -0400

    testing graph posting

--- main/main.js
+++ main/main.js
@@ -70,3 +70,9 @@ function setTicker(){
 
     runPrediction();
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelectorAll('img').forEach(function(img){
+       img.onerror = function(){this.style.display='none';};
+    })
+ });
